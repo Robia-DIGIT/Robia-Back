335a81b4ca048c3fbad8cc283c632efe96b1c88d
Nirina-fifalin <nirin.fifalin@gmail.com>
2026-08-06 21:20:56 +0300

    Correction CORS

--- src/main.ts
+++ src/main.ts
@@ -36,12 +36,32 @@ const allowedOrigins = [
 //  credentials: true,
 //});
 
+  app.use((req, res, next) => {
+    const origin = req.headers.origin;
+    
+    if (allowedOrigins.includes(origin)) {
+      res.header('Access-Control-Allow-Origin', origin);
+    }
+    
+    res.header('Access-Control-Allow-Credentials', 'true');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+
+    res.header('Vary', 'Origin'); 
+    
+    if (req.method === 'OPTIONS') {
+      return res.status(200).end();
+    }
+    
+    next();
+  });
+
   app.enableCors({
     origin: allowedOrigins,
     credentials: true,
   });
 
-  app.useGlobalPipes(
+app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
       forbidNonWhitelisted: true,
