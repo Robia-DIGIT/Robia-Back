0aa3a9e8e89533cb71ad38803e80caf5573d37d1
Nirina-fifalin <nirin.fifalin@gmail.com>
2026-08-06 11:07:44 +0300

    Simple correction

--- src/app.service.ts
+++ src/app.service.ts
@@ -9,7 +9,7 @@ export class AppService {
       <head>
           <meta charset="UTF-8">
           <meta name="viewport" content="width=device-width, initial-scale=1.0">
-          <title>Statut de l'API</title>
+          <title>API Status</title>
           <style>
               body {
                   font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
