94c579b6422f48a13b394363e383c7953e277956
robia-bit <hello@robia.digital>
2026-09-13 16:21:55 +0200

    fix(rc14): suppress unsafe mock return in execution spec

--- src/action-items/action-execution.service.spec.ts
+++ src/action-items/action-execution.service.spec.ts
@@ -1,4 +1,4 @@
-/* eslint-disable @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-argument */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-argument, @typescript-eslint/no-unsafe-return */
 import { BadRequestException, NotFoundException } from '@nestjs/common';
 import { ActionExecutionService } from './action-execution.service';
 
