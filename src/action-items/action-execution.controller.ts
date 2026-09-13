94ef126404c5b3f8ddb8e88c0338ef52b2ad6cd5
robia-bit <hello@robia.digital>
2026-09-13 16:25:15 +0200

    fix(rc14): format execution controller

--- src/action-items/action-execution.controller.ts
+++ src/action-items/action-execution.controller.ts
@@ -1,4 +1,12 @@
-import { Body, Controller, Get, Param, Post, Req, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  Post,
+  Req,
+  UseGuards,
+} from '@nestjs/common';
 import { JwtAuthGuard } from '../common/guards/jwt-auth.guard';
 import { OrgScopeGuard } from '../common/guards/org-scope.guard';
 import { ActionExecutionService } from './action-execution.service';
